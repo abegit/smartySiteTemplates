bb93130de7b1c4d8f700c868379f70fb45274049
Abraham Perez <abe@unscene.us>
2014-10-23 17:07:05 -0700

    no message

---
+++ _gsdata_/_saved_/rootfiles/members/_GREGindex.php
@@ -0,0 +1,142 @@
+<!DOCTYPE html>
+
+<!-- paulirish.com/2008/conditional-stylesheets-vs-css-hacks-answer-neither/ -->
+<!--[if lt IE 7]> <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang="en"> <![endif]-->
+<!--[if IE 7]>    <html class="no-js lt-ie9 lt-ie8" lang="en"> <![endif]-->
+<!--[if IE 8]>    <html class="no-js lt-ie9" lang="en"> <![endif]-->
+<!--[if gt IE 8]><!--> <html lang="en"> <!--<![endif]-->
+<head>
+  <meta charset="utf-8" />
+
+  <!-- Set the viewport width to device width for mobile -->
+  <meta name="viewport" content="width=device-width" />
+
+  <title>Bonoboville - Social Media Grown Up</title>
+
+  <!-- Included CSS Files -->
+<link rel="stylesheet" type="text/css" href="/about/ico.css">
+<link rel="stylesheet" type="text/css" href="/about/styles.css">
+<link rel="stylesheet" type="text/css" href="/about/icos.css">
+<link rel="stylesheet" type="text/css" href="/about/abedits.css">
+<link href='http://fonts.googleapis.com/css?family=Lato:900|Lora:400,700,400italic,700italic|Quattrocento:400,700' rel='stylesheet' type='text/css'>
+<style>
+body {
+    background: url("/about/images/map.jpg") repeat fixed 0 0 / cover rgba(0, 0, 0, 0);
+}
+.enter {background: #f8f2da; /* Old browsers */
+/* IE9 SVG, needs conditional override of 'filter' to 'none' */
+background: url(data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiA/Pgo8c3ZnIHhtbG5zPSJodHRwOi8vd3d3LnczLm9yZy8yMDAwL3N2ZyIgd2lkdGg9IjEwMCUiIGhlaWdodD0iMTAwJSIgdmlld0JveD0iMCAwIDEgMSIgcHJlc2VydmVBc3BlY3RSYXRpbz0ibm9uZSI+CiAgPHJhZGlhbEdyYWRpZW50IGlkPSJncmFkLXVjZ2ctZ2VuZXJhdGVkIiBncmFkaWVudFVuaXRzPSJ1c2VyU3BhY2VPblVzZSIgY3g9IjUwJSIgY3k9IjUwJSIgcj0iNzUlIj4KICAgIDxzdG9wIG9mZnNldD0iMCUiIHN0b3AtY29sb3I9IiNmOGYyZGEiIHN0b3Atb3BhY2l0eT0iMSIvPgogICAgPHN0b3Agb2Zmc2V0PSIxMDAlIiBzdG9wLWNvbG9yPSIjZjZlMzk0IiBzdG9wLW9wYWNpdHk9IjEiLz4KICA8L3JhZGlhbEdyYWRpZW50PgogIDxyZWN0IHg9Ii01MCIgeT0iLTUwIiB3aWR0aD0iMTAxIiBoZWlnaHQ9IjEwMSIgZmlsbD0idXJsKCNncmFkLXVjZ2ctZ2VuZXJhdGVkKSIgLz4KPC9zdmc+);
+background: -moz-radial-gradient(center, ellipse cover, rgba(248, 242, 218,1) 0%, rgba(246, 227, 148,.2) 100%); /* FF3.6+ */
+background: -webkit-gradient(radial, center center, 0px, center center, 100%, color-stop(0%,rgba(248, 242, 218,1)), color-stop(100%,rgba(246, 227, 148,.2))); /* Chrome,Safari4+ */
+background: -webkit-radial-gradient(center, ellipse cover, rgba(248, 242, 218,1) 0%,rgba(246, 227, 148,.2) 100%); /* Chrome10+,Safari5.1+ */
+background: -o-radial-gradient(center, ellipse cover, rgba(248, 242, 218,1) 0%,rgba(246, 227, 148,.2) 100%); /* Opera 12+ */
+background: -ms-radial-gradient(center, ellipse cover, rgba(248, 242, 218,1) 0%,rgba(246, 227, 148,.2) 100%); /* IE10+ */
+background: radial-gradient(ellipse at center, rgba(248, 242, 218,1) 0%,rgba(246, 227, 148,.2) 100%); /* W3C */
+filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#f8f2da', endColorstr='#f6e394',GradientType=1 ); /* IE6-8 fallback on horizontal gradient */
+}
+
+.home {background:url('images/dig.png');}
+.row {
+    margin: 0 auto;
+    max-width: 1400px;
+    padding:3%;
+
+}
+a, span {
+    font-family: oldd;
+    font-style: normal;
+    color:#7a5803;
+    text-decoration: none;
+    font-size: 1.4em;
+}
+.text-center {text-align: center;}
+.choice a {
+    border: 3px solid #9A7823;
+    margin: 0 10px;
+    padding: 10px 50px; display:inline-block;
+    margin-bottom:10px;
+}
+.signup {background: none repeat scroll 0 0 #9E6B36;
+    box-shadow: 0 2px 0 #CE9B66 inset;
+    color: #F4CE88;
+    text-shadow: 0 2px 0 #845E18;}
+.choice a:hover {
+    background:#6A4800;
+    color:#fff;
+    box-shadow: none;
+}
+
+
+
+
+
+
+video#bgvid { 
+
+background: none repeat scroll 0 0 rgba(0, 0, 0, 0);
+    border: medium none;
+    bottom: 0;
+    height: 50% !important;
+    padding: 7% 4% 6%;
+    position: relative;
+    right: 0;
+    width: 76.1% !important;
+    z-index: 1;
+}
+
+
+.pulsemaps-widget {transform: scale(1.8);-webkit-transform: scale(1.8);-ms-transform:scale(1.8);margin-top: 120px;}
+.pulsemaps-widget > div {background:url('http://bonoboville.com/about/images/map.png') no-repeat scroll center bottom rgba(0, 0, 0, 0) !important;background-size: contain !important;}
+
+
+  </style>
+
+  <script type="text/javascript" src="script.js"></script>
+
+
+  <!--[if lt IE 9]>
+  <link rel="stylesheet" href="stylesheets/ie.css">
+  <![endif]-->
+
+
+  <!-- IE Fix for HTML5 Tags -->
+  <!--[if lt IE 9]>
+  <script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+  <![endif]-->
+</head>
+
+<body>
+<div class="enter" style="min-height: 100vh;">
+  <div class="container home" style="min-height: 100vh;">
+
+  <div class="row">
+      <div style="text-align:right;"></div>
+  </div>
+
+  <div class="container"> <div class="row">
+  <div class="large-12 text-center"><img style="max-width: 31%;" src="/about/images/bologo-3heads.png"><br>
+  <img style="max-width: 50%;" src="/about/images/bonoboville-logo.png">
+  <br>
+  <img style="max-width: 41%;" src="/about/images/arrow.png"><br>
+  <span>Social Media Grown Up</span>
+  
+
+  </div>
+  </div>
+  </div>
+
+  <div class="container choice text-center"> <div class="row">
+    <div class="large-12 text-center">
+      <span style="margin-bottom: 10px; display: inline-block;">We will be back shortly,<br>somebody left the server under the coconut tree. <br><br> </span><br>
+      
+
+  </div></div>
+  </div>
+
+  <div class="row">
+      
+  </div>
+</div> </div>
+</body>
+</html>
+
